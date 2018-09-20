36b5037963ec0e25d8f7100e885969021fcc94af
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-09-20 16:23:37 -0400

    initial commit

---
+++ src/data/sectionListData.js
@@ -0,0 +1,49 @@
+var sectionListData = [
+    {
+        title: "Server Side",
+        data: [
+            {
+                name: 'Django',
+                description: 'Django is a high level Python Web framework'
+            },
+            {
+                name: 'Express',
+                description: 'Express is a fast, unopinionated, flexible and minimalist web framework for Node.js'
+            },
+            {
+                name: 'Ruby on Rails',
+                description: 'Rails (usually referred to as "Ruby on Rails") is a web framework written for the Ruby programming language.'
+            },
+        ]
+    },
+    {
+        title: 'Client Side',
+        data: [
+            {
+                name: 'AngularJS',
+                description: 'An incredibly robus JavaScript framework for data-heavy sites',
+            },
+            {
+                name: 'jQuery',
+                description: 'A fast, small, JS object library that streamlines how JavaScript behaves across different browsers'
+            },
+            {
+                name: 'React',
+                description: 'for user interface design'
+            }
+        ]
+    },
+    {
+        title: "Artificial Intelligence",
+        data: [
+            {
+                name: 'TensorFlow',
+                description: 'An open-source software library for Machine Intelligence.'
+            },
+            {
+                name: 'Wise.io',
+                description: "Wise.io also aims to democratise the use of artificial intelligence with 'machine learning as a service"
+            }
+        ]
+    }
+]
